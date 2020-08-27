12eafea05afb93776aef0339e00edf39f6b5341c
David Reyes <hdr.ligna@yavirac.edu.ec>
2020-08-27 09:47:24 -0500

    Deber  data-driven unit test

---
+++ Matematicas/Matematica.Negocio/Calculadora.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Matematica
+{
+    public class Calculadora
+    {
+        public int Sumar(int numero1, int numero2, int numero3)
+        {
+            return numero1 + numero2 + numero3;
+        }
+    }
+}
---
+++ Matematicas/Matematica.Test/CalculadoraTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Matematica.Test
+{
+    [TestClass]
+    public class CalculadoraTest
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\data.csv", "data#csv", DataAccessMethod.Sequential),
+            DeploymentItem("data.csv")]
+
+        public void SumaDeTresNumerosEnteros()
+        {
+            var numero1 = Int32.Parse(TestContext.DataRow["numero1"].ToString());
+            var numero2 = Int32.Parse(TestContext.DataRow["numero2"].ToString());
+            var numero3 = Int32.Parse(TestContext.DataRow["numero3"].ToString());
+            var esperado = Int32.Parse(TestContext.DataRow["resultado"].ToString());
+
+            var calcular = new Calculadora();
+            var resultado = calcular.Sumar(numero1, numero2, numero3);
+
+            Assert.AreEqual(esperado, resultado);
+        }
+    }
+}
